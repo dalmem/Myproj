a48da90b84c139bc424e6dd2feeca058de187c58
dalmem <61464129+dalmem@users.noreply.github.com>
2021-05-24 17:47:01 +0900

    day1

---
+++ practice01/src/main/java/practice01/MainClass.java
@@ -0,0 +1,19 @@
+package practice01;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+//		SpringTest st = new SpringTest();
+//		st.method();
+		
+		GenericXmlApplicationContext ct = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		
+		SpringTest test = ct.getBean("good",SpringTest.class);
+		test.method();
+		
+		ct.close();
+	}
+}
---
+++ practice01/src/main/java/practice01/SpringTest.java
@@ -0,0 +1,8 @@
+package practice01;
+
+public class SpringTest {
+	
+	public void method() {
+		System.out.println("스프링 복습");
+	}
+}
